4653560b9fe99dc6bb171d067a80cd2d768f9656
sol993 <sol993@gmail.com>
2016-08-22 21:19:17 -0300

    Sacar el porcentaje de cuantas veces salen del 0 al 9

--- index.php
+++ index.php
@@ -25,37 +25,42 @@
 	$con7=0;
 	$con8=0;
 	$con9=0;
-for ($i=0; $i < $Veces ; $i++) { $Ran=mt_rand(0,9);
+for ($i=0; $i < $Veces ; $i++) { $Ran=mt_rand(0,100);
 	switch ($Ran) {
 			case '0':
-			# code...
+			 $con0=$con0+1;
 			break;
 			case '1':
-			# code...
+			$con1=$con1+1;
 			break;
 			case '2':
-			# code...
+			$con2=$con2+1;
 			break;
 			case '3':
-			# code...
+			$con3=$con3+1;
 			break;
 			case '4':
-			# code...
+			$con4=$con4+1;
 			break;
 			case '5':
 			# code...
+			$con5=$con5+1;
 			break;
 			case '6':
 			# code...
+			$con6=$con6+1;
 			break;
 			case '7':
 			# code...
+			$con7=$con7+1;
 			break;
 			case '8':
 			# code...
+			$con8=$con8+1;
 			break;
 			case '9':
 			# code...
+			$con9=$con9+1;
 			break;
 		
 		default:
@@ -68,6 +73,32 @@ for ($i=0; $i < $Veces ; $i++) { $Ran=mt_rand(0,9);
 	echo "$Ran";
 
 }
+echo "<br>";
+$aux=0;$aux=($con0*100)/10;
+echo 'El numero 0 salio %'.$aux;
+echo "<br>";
+$aux=0;$aux=($con1*100)/10;
+echo 'El numero 1 salio %'.$aux;
+echo "<br>";
+$aux=0;$aux=($con2*100)/10;
+echo 'El numero 2 salio %'.$aux;
+echo "<br>";
+$aux=0;$aux=($con3*100)/10;
+echo 'El numero 3 salio %'.$aux;
+echo "<br>";
+$aux=0;$aux=($con4*100)/10;
+echo 'El numero 4 salio %'.$aux;
+echo "<br>";
+$aux=0;$aux=($con5*100)/10;
+echo 'El numero 5 salio %'.$aux;echo "<br>";
+$aux=0;$aux=($con6*100)/10;
+echo 'El numero 6 salio %'.$aux;echo "<br>";
+$aux=0;$aux=($con7*100)/10;
+echo 'El numero 7 salio %'.$aux;echo "<br>";
+$aux=0;$aux=($con8*100)/10;
+echo 'El numero 8 salio %'.$aux;echo "<br>";
+$aux=0;$aux=($con9*100)/10;
+echo 'El numero 9 salio %'.$aux;echo "<br>";
 	
 
 ?>
